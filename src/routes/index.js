7c1181a43ab4ec3d63255f659573b6f7d3efbe3b
TanThanh-CityBoi <19522239@gm.uit.edu.vn>
2021-10-23 10:50:30 +0700

    add file HomeController, homehbs, index.js, home.js, folder routes and install exphbs, mongoes..

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const homeRouter = require('./home')
+
+
+function route(app) {
+    app.get('/', (req, res) => {
+        res.render('home');
+    })
+    app.use('/home', homeRouter);
+}
+
+module.exports = route;
