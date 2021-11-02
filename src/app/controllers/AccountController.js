3e37440b47ccdac5d9a9d9fd21a8cabad1a94883
Quang Hoàng Dận <19522092@ms.uit.edu.vn>
2021-11-02 15:27:48 +0700

    update account UI screen

--- src/app/controllers/AccountController.js
+++ src/app/controllers/AccountController.js
@@ -1,9 +1,48 @@
-// const res = require("express/lib/response");
+const res = require("express/lib/response");
+const TaiKhoan = require('./../models/taikhoan');
 
 class AccountController {
     index(req, res) {
+        
+        console.log('index');
+
+        // var account = new TaiKhoan({
+        //     TenDangNhap: 'hoangdanquang',
+        //     MatKhau: 'matkhau',
+        //     HoTen: 'Hoàng Dận Quang',
+        //     SoDienThoai: '0123456789',
+        //     Mail: 'hoangdanquang@gmail.com'
+        // });
+
+        // account.save().then(function() {
+        //     if (account.isNew === false) {
+        //         TaiKhoan.findOne({ TenDangNhap: 'hoangdanquang'}).then(function(result) {
+        //             console.log(result);
+        //         });
+        //     }
+        //     else console.log('cannot find account');
+        // });
+
+        
         res.render('account');
     }
+
+    info(req, res) {
+
+        console.log('info');
+
+        var accountInfo = TaiKhoan.findOne({ TenDangNhap: 'hoangdanquang'}).then(function(data) {
+            if (data) {
+                console.log(data);
+                res.redirect(data);
+            }
+            else {
+                console.log('cannot find account info');
+                res.redirect(account);
+            }
+        });
+    }
+
 }
 
 module.exports = new AccountController;
--- src/routes/account.js
+++ src/routes/account.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const accountController = require('../app/controllers/AccountController');
 
+router.get('/info', accountController.info);
 router.use('/', accountController.index);
 
 
