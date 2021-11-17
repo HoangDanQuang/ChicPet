a186f64f9f68b4547ddfdc69491bb68c072510f0
TanThanh-CityBoi <19522239@gm.uit.edu.vn>
2021-11-17 15:11:20 +0700

    code backend book service

--- src/routes/service.js
+++ src/routes/service.js
@@ -2,8 +2,8 @@ const express = require('express');
 const ServiceController = require('../app/controllers/ServiceController');
 const router = express.Router();
 
-
 router.get('/book', ServiceController.book_get);
+router.post('/book', ServiceController.book_post);
 router.get('/', ServiceController.service_get);
 
 
